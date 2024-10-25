97a42f2ac0a4589dfb76b63dabc95f4e2fb11393
ricardo <xtx_scau2015@163.com>
2024-10-25 23:49:55 +0800

    build an empty project

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+/*
+ * @author: ricardo
+ * @brief: This file contains the declaration...
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "hello word." << std::endl;
+  return 0;
+}
